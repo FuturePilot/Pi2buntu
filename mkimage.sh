9f136d0656b4acd52a42e80d09db9e10d091169c
FuturePilot <futurepilot@gmx.us>
2015-03-16 23:50:53 -0400

    Add mkimage.sh

---
+++ mkimage.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+########################################################################
+# rpi2-build-image
+# Copyright (C) 2015 Ryan Finnie <ryan@finnie.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+########################################################################
+
+set -e
+set -x
+
+RELEASE=utopic
+BASEDIR=/srv/rpi2/${RELEASE}
+BUILDDIR=${BASEDIR}/build
+SHELL=/bin/bash
+
+# Set up environment
+export TZ=UTC
+R=${BUILDDIR}/chroot
+
+# Unmount mounted filesystems
+umount $R/proc || true
+umount $R/sys || true
+
+# Clean up files
+rm -f $R/etc/apt/sources.list.save
+rm -f $R/etc/resolvconf/resolv.conf.d/original
+rm -rf $R/run
+mkdir -p $R/run
+rm -f $R/etc/*-
+rm -f $R/root/.bash_history
+rm -rf $R/tmp/*
+rm -f $R/var/lib/urandom/random-seed
+[ -L $R/var/lib/dbus/machine-id ] || rm -f $R/var/lib/dbus/machine-id
+rm -f $R/etc/machine-id
+rm -f $R/usr/bin/qemu-arm-static
+rm -f $R/etc/ssh/ssh_host_*
+
+# Build the image file
+# Currently hardcoded to a 1.75GiB image
+DATE="$(date +%Y-%m-%d)"
+dd if=/dev/zero of="$BASEDIR/${DATE}-ubuntu-${RELEASE}.img" bs=1M count=1
+dd if=/dev/zero of="$BASEDIR/${DATE}-ubuntu-${RELEASE}.img" bs=1M count=0 seek=1792
+sfdisk -f "$BASEDIR/${DATE}-ubuntu-${RELEASE}.img" <<EOM
+unit: sectors
+
+1 : start=     2048, size=   131072, Id= c, bootable
+2 : start=   133120, size=  3536896, Id=83
+3 : start=        0, size=        0, Id= 0
+4 : start=        0, size=        0, Id= 0
+EOM
+VFAT_LOOP="$(losetup -o 1M --sizelimit 64M -f --show $BASEDIR/${DATE}-ubuntu-${RELEASE}.img)"
+EXT4_LOOP="$(losetup -o 65M --sizelimit 1727M -f --show $BASEDIR/${DATE}-ubuntu-${RELEASE}.img)"
+mkfs.vfat "$VFAT_LOOP"
+mkfs.ext4 "$EXT4_LOOP"
+MOUNTDIR="$BUILDDIR/mount"
+mkdir -p "$MOUNTDIR"
+mount "$EXT4_LOOP" "$MOUNTDIR"
+mkdir -p "$MOUNTDIR/boot/firmware"
+mount "$VFAT_LOOP" "$MOUNTDIR/boot/firmware"
+rsync -a "$R/" "$MOUNTDIR/"
+umount "$MOUNTDIR/boot/firmware"
+umount "$MOUNTDIR"
+losetup -d "$EXT4_LOOP"
+losetup -d "$VFAT_LOOP"
+if which bmaptool; then
+  bmaptool create -o "$BASEDIR/${DATE}-ubuntu-${RELEASE}.bmap" "$BASEDIR/${DATE}-ubuntu-${RELEASE}.img"
+fi
