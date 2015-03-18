8762546ec1af830843fe7f21095679084a0843f9
FuturePilot <futurepilot@gmx.us>
2015-03-18 19:18:04 -0400

    Change default hostname. Fixes issue #9

--- rpi2-build-image.sh
+++ rpi2-build-image.sh
@@ -116,14 +116,14 @@ proc            /proc           proc    defaults          0       0
 EOM
 
 # Set up hosts
-echo ubuntu >$R/etc/hostname
+echo raspberry-pi2 >$R/etc/hostname
 cat <<EOM >$R/etc/hosts
 127.0.0.1       localhost
 ::1             localhost ip6-localhost ip6-loopback
 ff02::1         ip6-allnodes
 ff02::2         ip6-allrouters
 
-127.0.1.1       ubuntu
+127.0.1.1       raspberry-pi2
 EOM
 
 # Set up default user
