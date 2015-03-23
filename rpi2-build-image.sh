f0c5acd44b17f9180394a907a9097bd9456511f7
FuturePilot <futurepilot@gmx.us>
2015-03-22 20:03:34 -0400

    Split firmware config.txt out of main script

--- rpi2-build-image.sh
+++ rpi2-build-image.sh
@@ -101,51 +101,7 @@ chroot $R $SHELL -c "apt-get clean"
 cat ./configs/interfaces > $R/etc/network/interfaces
 
 # Set up firmware config
-cat <<EOM >$R/boot/firmware/config.txt
-# For more options and information see 
-# http://www.raspberrypi.org/documentation/configuration/config-txt.md
-# Some settings may impact device functionality. See link above for details
-
-# uncomment if you get no picture on HDMI for a default "safe" mode
-#hdmi_safe=1
-
-# uncomment this if your display has a black border of unused pixels visible
-# and your display can output without overscan
-#disable_overscan=1
-
-# uncomment the following to adjust overscan. Use positive numbers if console
-# goes off screen, and negative if there is too much border
-#overscan_left=16
-#overscan_right=16
-#overscan_top=16
-#overscan_bottom=16
-
-# uncomment to force a console size. By default it will be display's size minus
-# overscan.
-#framebuffer_width=1280
-#framebuffer_height=720
-
-# uncomment if hdmi display is not detected and composite is being output
-#hdmi_force_hotplug=1
-
-# uncomment to force a specific HDMI mode (this will force VGA)
-#hdmi_group=1
-#hdmi_mode=1
-
-# uncomment to force a HDMI mode rather than DVI. This can make audio work in
-# DMT (computer monitor) modes
-#hdmi_drive=2
-
-# uncomment to increase signal to HDMI, if you have interference, blanking, or
-# no display
-#config_hdmi_boost=4
-
-# uncomment for composite PAL
-#sdtv_mode=2
-
-#uncomment to overclock the arm. 700 MHz is the default.
-#arm_freq=800
-EOM
+cat ./configs/config.txt > $R/boot/firmware/config.txt
 ln -sf firmware/config.txt $R/boot/config.txt
 echo 'dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 rootwait' > $R/boot/firmware/cmdline.txt
 ln -sf firmware/cmdline.txt $R/boot/cmdline.txt
