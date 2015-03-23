99f4059ec021d5d8dcfef90a20766502c02ca4ea
FuturePilot <futurepilot@gmx.us>
2015-03-22 20:00:25 -0400

    Split interfaces out of main script

--- rpi2-build-image.sh
+++ rpi2-build-image.sh
@@ -98,19 +98,7 @@ chroot $R $SHELL -c "chown 1000:1000 /home/pi/.profile*"
 chroot $R $SHELL -c "apt-get clean"
 
 # Set up interfaces
-cat <<EOM >$R/etc/network/interfaces
-# interfaces(5) file used by ifup(8) and ifdown(8)
-# Include files from /etc/network/interfaces.d:
-source-directory /etc/network/interfaces.d
-
-# The loopback network interface
-auto lo
-iface lo inet loopback
-
-# The primary network interface
-allow-hotplug eth0
-iface eth0 inet dhcp
-EOM
+cat ./configs/interfaces > $R/etc/network/interfaces
 
 # Set up firmware config
 cat <<EOM >$R/boot/firmware/config.txt
